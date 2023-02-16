88e85386f43b6595fda1a33b133d1ef6c9dd3ab6
Pasq097 <s294872@studenti.polito.it>
2023-02-16 20:16:26 +0100

    11-8 sub menu 13:35 almost finished, error I don't see the 3 sliders don't know why yet

--- core/demo.py
+++ core/demo.py
@@ -163,22 +163,22 @@ class FrontEnd:
 
     def blur_action(self):
         self.refresh_side_frame()
-        ttk.Label(
-            self.side_frame, text="Averaging Blur"
-        ).grid(row=0, column=2, padx=5, sticky='sw')
 
-        self.average_slider = Scale(
+        ttk.Label(
+            self.side_frame, text="Averaging Blur").grid(row=0, column=2, padx=5, sticky='sw')
+        self.average_slider = ttk.Scale(
             self.side_frame, from_=0, to=256, orient=HORIZONTAL, command=self.averaging_action)
         self.average_slider.grid(row=1, column=2, padx=5, sticky='sw')
 
-        ttk.Label(
-            self.side_frame, text="Gaussian Blur"
-        ).grid(row=2, colum=2, padx=5, sticky='sw')
-
+        ttk.Label(self.side_frame, text="Gaussian Blur").grid(row=2, colum=2, padx=5, sticky='sw')
         self.gaussian_slider = Scale(
-            self.side_frame, from_=0, to=256, orient=HORIZONTAL, command=self.gaussian_action
-        )
+            self.side_frame, from_=0, to=256, orient=HORIZONTAL, command=self.gaussian_action)
         self.gaussian_slider.grid(row=3, column=2, padx=5, sticky='sw')
+
+        ttk.Label(self.side_frame, text="Median Blur").grid(row=4, column=2, padx=5, sticky='sw')
+        self.median_slider = Scale(
+            self.side_frame, from_=0, to=256, orient=HORIZONTAL, command=self.median_action)
+        self.median_slider.grid(row=5, column=2,padx=5,sticky='sw')
 
 
     def rotate_action(self):
@@ -232,6 +232,8 @@ class FrontEnd:
         pass
 
     def gaussian_action(self):
+        pass
+    def median_action(self):
         pass
 
 root = Tk()
